026893f005d6008081ee08b386bf31df5d50aeaf
waking777 <756274132@qq.com>
2019-10-10 11:50:39 +0800

    20191010上午摸鱼练习

---
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户持久层接口
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有的操作
+     * @return
+     */
+    List<User> findAll();
+}
